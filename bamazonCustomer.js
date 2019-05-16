d8521e919d0916898a5f76da902e5d3e637bcd6d
unknown <mauro.hdz99@gmail.com>
2019-05-16 18:08:21 -0500

    Successfully connected the db to the js file!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,24 @@
 const inquirer = require("inquirer");
 
-inquirer
+const mysql = require("mysql");
+
+// Sets up the connection to the database
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : 3306,
+    user     : 'me',
+    password : 'secret',
+    database : 'bamazon_DB'
+  });
+   
+  connection.connect(function(err){
+        console.log("connected as id:"+connection.threadId);
+  });
+
+  inquirer
   .prompt([
     /* Pass your questions in here */
+    
   ])
   .then(answers => {
     // Use user feedback for... whatever!!
